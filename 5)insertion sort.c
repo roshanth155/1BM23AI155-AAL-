746461dd4026834e08e9813be08f93808a65b2df
roshanth155 <roshanthv.ai23@bmsce.ac.in>
2025-05-13 10:17:16 +0530

    Update and rename insertion sort.c to 5)insertion sort.c

--- 5)insertion sort.c
+++ 5)insertion sort.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 
 void insertionSort(int arr[], int n) {
-    for (int i = 1; i < n; i++) {
-        int key = arr[i], j = i - 1;
-        while (j >= 0 && arr[j] > key) arr[j + 1] = arr[j], j--;
+    int i, j, key;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        for (j = i - 1; j >= 0 && arr[j] > key; j--)
+            arr[j + 1] = arr[j];
         arr[j + 1] = key;
     }
 }
 
 void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    int i;
+    for (i = 0; i < n; i++) 
+        printf("%d ", arr[i]);
     printf("\n");
 }
 
 int main() {
     int arr[] = {12, 11, 13, 5, 6};
     int n = sizeof(arr) / sizeof(arr[0]);
-    
+
     printArray(arr, n);
     insertionSort(arr, n);
     printArray(arr, n);
 
     return 0;
 }
+
