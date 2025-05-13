3f3c4802011377102f29772680ce01e75165cb75
roshanth155 <roshanthv.ai23@bmsce.ac.in>
2025-05-13 09:51:18 +0530

    Updated selctionsort.c

--- selctionsort.c
+++ selctionsort.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 
-
 void selectionSort(int arr[], int n) {
-    int i, j, minIndex, temp;
+    int i, j, minIndex, temp;  // Declare i, j, minIndex, and temp outside the loop
 
-    
     for (i = 0; i < n - 1; i++) {
-        
         minIndex = i;
         for (j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
+            if (arr[j] < arr[minIndex]) minIndex = j;
         }
-
-        
         if (minIndex != i) {
             temp = arr[i];
             arr[i] = arr[minIndex];
@@ -23,26 +16,20 @@ void selectionSort(int arr[], int n) {
     }
 }
 
-
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);  
-    }
-    printf("\n");
-}
-
 int main() {
     int arr[] = {64, 25, 12, 22, 11};
     int n = sizeof(arr) / sizeof(arr[0]);
+    int i;  // Declare i outside the loops for compatibility with C90
 
-    printf("Original array: \n");
-    printArray(arr, n);  
+    printf("Original array: ");
+    for (i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
 
-    selectionSort(arr, n);  
+    selectionSort(arr, n);
 
-    printf("Sorted array: \n");
-    printArray(arr, n);  
+    printf("Sorted array: ");
+    for (i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
 
-    getchar();  
     return 0;
 }
